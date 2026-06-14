e4da994443e85e1123632317b75c88487387f146
Streak Official <20pritchekaitlin@gmail.com>
2026-06-14 16:07:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 const API = "https://streak-bot-9vnn.onrender.com/api/settings";
 
 // LOAD
-async function loadSettings() {
-  const res = await fetch(API);
-  const data = await res.json();
+async function load() {
+  try {
+    const res = await fetch(API);
+    const data = await res.json();
 
-  const inputs = document.querySelectorAll("input");
+    const inputs = document.querySelectorAll("input");
 
-  if (data.botName) inputs[0].value = data.botName;
-  if (data.prefix) inputs[1].value = data.prefix;
+    if (data.botName) inputs[0].value = data.botName;
+    if (data.prefix) inputs[1].value = data.prefix;
+    if (data.status) inputs[2].value = data.status;
+
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 // SAVE
-async function saveSettings() {
+async function save() {
   const inputs = document.querySelectorAll("input");
 
   await fetch(API, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      botName: inputs[0].value,
-      prefix: inputs[1].value
+      botName: inputs[0]?.value,
+      prefix: inputs[1]?.value,
+      status: inputs[2]?.value
     })
   });
 
@@ -28,6 +35,6 @@ async function saveSettings() {
 }
 
 // BUTTON
-document.querySelector(".save-btn").onclick = saveSettings;
+document.querySelector(".save-btn")?.addEventListener("click", save);
 
-loadSettings();
+load();
